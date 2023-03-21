850d6457cc475b3f9dec2a1623b74d6205888966
Polishevskiy <78253233+Polishevskiy@users.noreply.github.com>
2023-03-21 20:21:19 +0200

    Lambda Practice
    First commit

---
+++ lambda/Test2.java
@@ -0,0 +1,16 @@
+package lambda;
+
+public class Test2 {
+    static void def(I i) {
+        System.out.println(i.abc("privet"));
+    }
+
+    public static void main(String[] args) {
+        int i = 10;
+        def((String s) -> {return s.length()+i;});
+    }
+}
+
+interface I {
+    int abc(String s);
+}
